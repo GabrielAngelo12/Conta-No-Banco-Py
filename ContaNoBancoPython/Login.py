36531268742809f245a2c5d28dd1039a6ed1c36c
GabrielAngelo12 <160635927+GabrielAngelo12@users.noreply.github.com>
2025-02-03 19:37:32 -0300

    Login.py

--- ContaNoBancoPython/Login.py
+++ ContaNoBancoPython/Login.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from Menu import Menu
+
 menu = Menu()
 
 class Conta:
@@ -14,12 +15,11 @@ class Conta:
         senha2 = getpass("CONFIRME A SENHA: ")
         self.cpf = input("Digite seu CPF: ")
         
-        if len(self.cpf) == 11 :
-            return self.cpf
-        else:
-            print("CPF inválido! O CPF deve ter 11 dígitos")
+        if len(self.cpf) != 11:
+            print("CPF inválido! O CPF deve ter 11 dígitos.")
             self.criar_conta()
-            
+            return
+        
         if self.senha == senha2:
             with open('arquivo.txt', 'w') as arquivo:
                 arquivo.write(f"NOME: {self.nome}\n")
@@ -28,32 +28,29 @@ class Conta:
             print("\nConta criada com sucesso!")
             self.login()
         else:
-            print("Erro! Senhas não coincidem. Voltando ao início...")
+            print("Erro! Senhas não coincidem. Voltando ao início...\n")
             self.criar_conta()
     
-    def login(self):
+    def login(self):   
         print("\nFaça login")
-        nome2 = input("Digite seu NOME: ")
-        senhaL = getpass("Digite a SENHA: ")
-        cpf2 = input("Digite seu CPF: ")
+        self.nome2 = input("Digite seu NOME: ")
+        self.senhaL = getpass("Digite a SENHA: ")
+        self.cpf2 = input("Digite seu CPF: ")
         
-        if len(self.cpf2) == 11 :
-            return self.cpf2
-        else:
-            print("CPF inválido! O CPF deve ter 11 dígitos")
+        if len(self.cpf2) != 11:
+            print("CPF inválido! O CPF deve ter 11 dígitos.")
             self.login()
-            
+            return
+        
         with open('arquivo.txt', 'r') as arquivo:
             dados = arquivo.readlines()
             nome_salvo = dados[0].split(":")[1].strip()
             senha_salva = dados[1].split(":")[1].strip()
-            cpf_salvo = int(dados[2].split(":")[1].strip())
-            
-        if nome2 == nome_salvo and senhaL == senha_salva and cpf2 == cpf_salvo:
+            cpf_salvo = dados[2].split(":")[1].strip() 
+        
+        if self.nome2 == nome_salvo and self.senhaL == senha_salva and self.cpf2 == cpf_salvo:
             print("\nLogin realizado com sucesso!")
             menu.escolher_opcao()
-            
         else:
             print("Dados incorretos! Tente novamente.")
-            self.login()  
-            
+            self.login()
